f5b609d8d304397b1ce723e414541955931e9256
andres hernandez <andrescodedev@gmail.com>
2024-11-13 18:57:06 -0500

    I finished to work open closed principle, implementing it with abstract classes and interfaces

--- open_closed/js/interface.js
+++ open_closed/js/interface.js
@@ -22,46 +22,24 @@ class Triangle {
     }
 }
 
-//SERVICES
-
-class InterfacePerimeterCalculatorService {
-    static perimeterCalculatorMethods = {
-        computePerimeter: function() {}
-    }
-
-    constructor() {
-        if (this.constructor === InterfacePerimeterCalculatorService) {
-            throw new Error(`You can't create an instance for an interface`);
-        }
-    }
-
-    static ensureInterfaceImplementation(template) {
-        for (const method in InterfacePerimeterCalculatorService.perimeterCalculatorMethods) {
-
-            if (!(method in template.prototype) ||
-                typeof template.prototype[method] !== "function") {
-                throw new Error(`The method ${method} must be implement in the class ${template.name}`);
-            }
-        }
+class Circle {
+    constructor(radius) {
+        this.PI = 3.14;
+        this.radius = radius;
     }
 }
 
-class InterfaceAreaCalculatorService {
-    //Define methods without functionality
-    static areaCalculatorMethods = {
-        computeArea: function () { }
-    }
-
-    //Hacer un contructor que no permita instanciar
-    constructor() {
-        if (this.constructor === InterfaceAreaCalculatorService) {
+//INTERFACES FOR SERVICES
+class InterfaceTemplate {
+    constructor(interfaceTemplate) {
+        if (this.constructor === interfaceTemplate) {
             throw new Error(`You can't create an instance for an interface`);
         }
     }
 
     //Method to ensure interface's methods implementation
-    static ensureInterfaceImplementation(template) {
-        for (const method in InterfaceAreaCalculatorService.areaCalculatorMethods) {
+    ensureInterfaceImplementation(template, interfaceMethods) {
+        for (const method in interfaceMethods) {
 
             if (!(method in template.prototype) ||
                 typeof template.prototype[method] !== "function") {
@@ -69,60 +47,139 @@ class InterfaceAreaCalculatorService {
             }
         }
     }
+}
+
+class InterfacePerimeterCalculatorService extends InterfaceTemplate {
+    perimeter;
+
+    #perimeterCalculatorMethods = {
+        computePerimeter: function () { }
+    }
+
+    constructor() {
+        super(InterfacePerimeterCalculatorService);
+    }
+
+    ensureInterfaceImplementation(template) {
+        super.ensureInterfaceImplementation(template, this.#perimeterCalculatorMethods);
+    }
+}
+
+class InterfaceAreaCalculatorService extends InterfaceTemplate {
+    area;
+
+    //Define methods without functionality
+    #areaCalculatorMethods = {
+        computeArea: function (geometricShape) { }
+    }
+
+    constructor() {
+        super(InterfaceAreaCalculatorService);
+    }
+
+    //Method to ensure interface's methods implementation
+    ensureInterfaceImplementation(template) {
+        super.ensureInterfaceImplementation(template, this.#areaCalculatorMethods);
+    }
 
 }
 
-class RectangleAreaCalculatorService {
+//SERVICES TO CALCULATE THE PERIMETER
+class RectanglePerimeterCalculatorService extends InterfacePerimeterCalculatorService {
     //Implementing an interface
     //This class must implement all the methods defined in the interface
     constructor() {
-        InterfaceAreaCalculatorService.ensureInterfaceImplementation(RectangleAreaCalculatorService);
+        super();
+        this.ensureInterfaceImplementation(RectanglePerimeterCalculatorService);
+    }
+
+    computePerimeter(rectangle) {
+        this.perimeter = (rectangle.width*2) + (rectangle.heigth*2);
+
+        return console.log(`The rectangle perimeter is ${this.perimeter}`);
+    }
+}
+
+//SERVICES TO CALCULATE THE AREA
+class RectangleAreaCalculatorService extends InterfaceAreaCalculatorService {
+    //Implementing an interface
+    //This class must implement all the methods defined in the interface
+    constructor() {
+        super();
+        this.ensureInterfaceImplementation(RectangleAreaCalculatorService);
     }
 
     computeArea(rectangle) {
-        let area = rectangle.width * rectangle.heigth;
+        this.area = rectangle.width * rectangle.heigth;
 
-        return console.log(`The rectangle area is ${area}`);
+        return console.log(`The rectangle area is ${this.area}`);
     }
 }
 
-class SquareAreaCalculatorService {
+class SquareAreaCalculatorService extends InterfaceAreaCalculatorService {
     constructor() {
-        InterfaceAreaCalculatorService.ensureInterfaceImplementation(SquareAreaCalculatorService);
+        super();
+        this.ensureInterfaceImplementation(SquareAreaCalculatorService);
     }
 
     computeArea(square) {
-        let area = square.width * square.width;
+        this.area = square.width * square.width;
 
-        return console.log(`The square area is ${area}`);
+        return console.log(`The square area is ${this.area}`);
     }
 }
 
-class TriangleAreaCalculatorService {
+class TriangleAreaCalculatorService extends InterfaceAreaCalculatorService {
     constructor() {
-        InterfaceAreaCalculatorService.ensureInterfaceImplementation(TriangleAreaCalculatorService);
+        super();
+        this.ensureInterfaceImplementation(TriangleAreaCalculatorService);
     }
 
     computeArea(triangle) {
-        let area = (triangle.width * triangle.heigth) / 2;
+        this.area = (triangle.width * triangle.heigth) / 2;
 
-        return console.log(`The triangle area is ${area}`);
+        return console.log(`The triangle area is ${this.area}`);
+    }
+}
+
+class CircleAreaCalculatorService extends InterfaceAreaCalculatorService {
+    constructor() {
+        super();
+        this.ensureInterfaceImplementation(CircleAreaCalculatorService);
+    }
+
+    computeArea(circle) {
+        this.area = circle.PI * (circle.radius ** 2);
+
+        return console.log(`The circle area is ${this.area}`);
     }
 }
 
 
+//INTANCES FOR THE PERIMETER SERVICES
+const rectanglePerimeterCalculatorService = new RectanglePerimeterCalculatorService();
+
+const rectangle = new Rectangle(2, 5);
+rectanglePerimeterCalculatorService.computePerimeter(rectangle);
+
+
+//INTANCES FOR THE AREA SERVICES
 const rectangleAreaCalculatorService = new RectangleAreaCalculatorService();
 const squareAreaCalculatorService = new SquareAreaCalculatorService();
 const triangleAreaCalculatorService = new TriangleAreaCalculatorService();
+const circleAreaCalculatorService = new CircleAreaCalculatorService();
 
-const rectangle = new Rectangle(2, 5);
-rectangleAreaCalculatorService.computeArea(rectangle);
+const rectangle2 = new Rectangle(2, 5);
+rectangleAreaCalculatorService.computeArea(rectangle2);
 
 const square = new Square(5);
 squareAreaCalculatorService.computeArea(square);
 
 const triangle = new Triangle(2, 9);
 triangleAreaCalculatorService.computeArea(triangle);
+
+const circle = new Circle(2);
+circleAreaCalculatorService.computeArea(circle);
 
 /*class InterfazMetodo {
     static interfaceMethods = {
