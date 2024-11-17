5e4c3cb1f530a2610f9f7bf81427966f8536409c
andres hernandez <andrescodedev@gmail.com>
2024-11-16 19:56:46 -0500

    I begin to understand the dependency inversion principle

---
+++ dependency_inversion/js/dependency_inversion.js
@@ -0,0 +1,9 @@
+/* 
+    DEPENDENCY INVERSION
+
+    - Los modulos de alto nivel, no deben depender de los modulos de bajo nivel. Ambos deben
+      depender de interfaces.
+
+    - Las interfaces no deben depender de los detalles, los detalles deben depender de las interfaces
+    
+*/
